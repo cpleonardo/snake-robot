7accf5c2e82a1abac0539bcc237d7c61c12db27b
TRA <pinedacesar94@hotmail.com>
2019-01-08 22:28:26 -0600

    S-005 | Create incremental joint publisher ros node
    	new file:   catkin_ws/src/snake_joint_state_publisher/scripts/joint_inc_publisher.py

---
+++ catkin_ws/src/snake_joint_state_publisher/scripts/joint_inc_publisher.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+import rospy
+from sensor_msgs.msg import Joy
+from snake_msgs.msg import Module
+import json
+
+
+class JointIncPublisher():
+
+    def __init__(self):
+        self.module = Module()
+        self.module.speed = 0
+        self.module.yaw = 0.0
+        self.module.pitch = 0.0
+        self.module.selected = 1
+        self.MAX_SPEED = 255
+        self.YAW_MULTIPLAYER = 1
+        self.PITCH_MULTIPLAYER = 1
+
+    def joyCallback(self, data):
+        right_trigger_value = data.axes[5]
+        left_button = data.buttons[4]
+        rigth_button = data.buttons[5]
+        self.module.yaw = data.axes[3] * self.YAW_MULTIPLAYER
+        self.module.pitch = data.axes[4] * self.PITCH_MULTIPLAYER
+        if rigth_button or left_button:
+            if rigth_button and self.module.selected != 7:
+                self.module.selected += 1
+            if left_button and self.module.selected != 1:
+                self.module.selected -= 1
+        if right_trigger_value != 1.0:
+            self.module.speed = int(
+                (1 - right_trigger_value) / 2 * self.MAX_SPEED)
+        print(json.dumps({
+            "speed": self.module.speed,
+            "pitch": self.module.pitch,
+            "yaw": self.module.yaw,
+            "active_module": self.module.selected
+        }, indent=4))
+
+    def start_node(self):
+        rospy.Subscriber("joy", Joy, self.joyCallback)
+        module_publisher = rospy.Publisher('snake_joint_inc',
+                                           Module,
+                                           queue_size=10)
+        rospy.init_node('snake_joint_inc', anonymous=True)
+        rate = rospy.Rate(10)  # 10hz
+        while not rospy.is_shutdown():
+            # rospy.loginfo(hello_str)
+            module_publisher.publish(self.module)
+            rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        joint_inc_pub = JointIncPublisher()
+        joint_inc_pub.start_node()
+    except rospy.ROSInterruptException:
+        pass
