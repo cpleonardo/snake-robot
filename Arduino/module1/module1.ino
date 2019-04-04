3621216e3302de897804865431cb3695afbd4ebb
protonauta <pinedacesar94@hotmail.com>
2019-04-04 02:09:45 -0600

    Simple SPEED and YAW pwm writer sketch

--- Arduino/module1/module1.ino
+++ Arduino/module1/module1.ino
@@ -1,147 +1,111 @@
-/*
- *  This sketch sends random data over UDP on a ESP32 device
- *
- */
-// #include <WiFi.h>
-// #include <WiFiUdp.h>
-// #include <ArduinoJson.h>
-//
-//
-// // WiFi network name and password:
-// const char * networkName = "ARRIS-2152";
-// const char * networkPswd = "848F706B6CFC6EAE";
-//
-// //IP address to send UDP data to:
-// // either use the ip address of the server or
-// // a network broadcast address
-// const char * udpAddress = "192.168.0.6";
-// const int udpPort = 3333;
-//
-// //Are we currently connected?
-// boolean connected = false;
-//
-// //The udp library class
-// WiFiUDP udp;
-//
-// void setup(){
-//   // Initilize hardware serial:
-//   Serial.begin(115200);
-//
-//   //Connect to the WiFi network
-//   connectToWiFi(networkName, networkPswd);
-// }
-//
-// void loop(){
-//   // Allocate JsonBuffer
-//   // Use arduinojson.org/assistant to compute the capacity.
-//   StaticJsonBuffer<500> jsonBuffer;
-//
-//   // Create the root object
-//   JsonObject& root = jsonBuffer.createObject();
-//
-//   root["module"] = 1;
-//
-//   JsonObject& data = root.createNestedObject("data");
-//   data["temp"] = 14.2;
-//   data["cond"] = "cloudy";
-//
-//   //only send data when connected
-//   if(connected){
-//     //Send a packet
-//     udp.beginPacket(udpAddress,udpPort);
-//     root.printTo(udp);
-//     // udp.printf("Seconds since boot: %u", millis()/1000);
-//     udp.println();
-//     udp.endPacket();
-//   }
-//   //Wait for 1 second
-//   delay(1000);
-// }
-//
-// void connectToWiFi(const char * ssid, const char * pwd){
-//   Serial.println("Connecting to WiFi network: " + String(ssid));
-//
-//   // delete old config
-//   WiFi.disconnect(true);
-//   //register event handler
-//   WiFi.onEvent(WiFiEvent);
-//
-//   //Initiate connection
-//   WiFi.begin(ssid, pwd);
-//
-//   Serial.println("Waiting for WIFI connection...");
-// }
-//
-// //wifi event handler
-// void WiFiEvent(WiFiEvent_t event){
-//     switch(event) {
-//       case SYSTEM_EVENT_STA_GOT_IP:
-//           //When connected set
-//           Serial.print("WiFi connected! IP address: ");
-//           Serial.println(WiFi.localIP());
-//           //initializes the UDP state
-//           //This initializes the transfer buffer
-//           udp.begin(WiFi.localIP(),udpPort);
-//           connected = true;
-//           break;
-//       case SYSTEM_EVENT_STA_DISCONNECTED:
-//           Serial.println("WiFi lost connection");
-//           connected = false;
-//           break;
-//     }
-// }
-
+#include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiMulti.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-const char* ssid = "ARRIS-2152";
-const char* password =  "848F706B6CFC6EAE";
+WiFiMulti wifiMulti;
+
+// Wifi name and password
+// const char* SSID = "Connectify-me";
+// const char* PASSWORD = "hola1234";
+const char* SSID = "INFINITUM689C_2.4";
+const char* PASSWORD = "9rUK2YTmQX";
+
+// Server IP and query params
+const String HOST = "http://192.168.1.136";
+const String PORT = "6666";
+const String MODULE = "/?module=1";
+
+// ESP32 PIN config
+
+// SPEED PINS
+const uint8_t SPEED_PWM_PIN = 26;
+const uint8_t SPEED_DIR_1 = 27;
+const uint8_t SPEED_DIR_2 = 14;
+const uint8_t SPEED_PWM_CHANNEL = 1;
+
+// YAW PINS
+const uint8_t YAW_PWM_PIN = 19;
+const uint8_t YAW_DIR_1 = 23;
+const uint8_t YAW_DIR_2 = 18;
+const uint8_t YAW_PWM_CHANNEL = 2;
 
 void setup() {
+  // SPEED
+  ledcAttachPin(SPEED_PWM_PIN, SPEED_PWM_CHANNEL);
+  ledcSetup(SPEED_PWM_CHANNEL, 12000, 8); // 12 kHz PWM, 8-bit resolution
+  pinMode(SPEED_DIR_1, OUTPUT);
+  pinMode(SPEED_DIR_2, OUTPUT);
+
+  // YAW
+  ledcAttachPin(YAW_PWM_PIN, YAW_PWM_CHANNEL);
+  ledcSetup(YAW_PWM_CHANNEL, 12000, 8); // 12 kHz PWM, 8-bit resolution
+  pinMode(YAW_DIR_1, OUTPUT);
+  pinMode(YAW_DIR_2, OUTPUT);
 
   Serial.begin(115200);
-  delay(4000);   //Delay needed before calling the WiFi.begin
+  Serial.println();
+  Serial.println();
+  Serial.println();
 
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+  for(uint8_t t = 4; t > 0; t--) {
+    Serial.print("[SETUP] WAIT ");
+    Serial.println(t);
+    Serial.flush();
     delay(1000);
-    Serial.println("Connecting to WiFi..");
   }
-
-  Serial.println("Connected to the WiFi network");
-
+  wifiMulti.addAP(SSID, PASSWORD);
 }
 
 void loop() {
+  // wait for WiFi connection
+  if(wifiMulti.run() != WL_CONNECTED) {
+    return;
+  }
+  HTTPClient http;
+  Serial.println("[HTTP] begin...");
+  http.begin(HOST + String(":") + PORT + MODULE);
+  Serial.println("[HTTP] GET...");
+  int httpCode = http.GET();
+  if(httpCode != HTTP_CODE_OK) {
+    Serial.print("[HTTP] GET... failed, error: ");
+    Serial.println(http.errorToString(httpCode).c_str());
+    http.end();
+    return;
+  }
 
- if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& response_msg = jsonBuffer.parseObject(http.getString());
 
-   HTTPClient http;
+  if(response_msg.success()) {
+    float speed = response_msg["speed"];
+    float yaw = response_msg["yaw"];
+    float pitch = response_msg["pitch"];
+    write_pwm_speed(speed);
+    write_pwm_yaw(yaw);
+  }
+  else {
+    Serial.println("parseObject() failed");
+  }
+  http.end();
+  delay(1);
+}
 
-   http.begin("http://192.168.0.6:3333");
-   http.addHeader("Content-Type", "application/json");
+void write_pwm_speed(float pwm) {
+  bool dir = pwm > 1 ? LOW : HIGH;
+  Serial.println(dir);
+  ledcWrite(SPEED_PWM_CHANNEL, abs(pwm));
+  digitalWrite(SPEED_DIR_1, dir);
+  digitalWrite(SPEED_DIR_2, !dir);
+  Serial.println(abs(pwm));
+}
 
-   int httpResponseCode = http.POST("POSTING from ESP32");
-
-   if(httpResponseCode>0){
-     String response = http.getString();
-     Serial.println(httpResponseCode);
-     Serial.println(response);
-
-   }else{
-     Serial.print("Error on sending POST: ");
-     Serial.println(httpResponseCode);
-   }
-
-   http.end();  //Free resources
-
- }else{
-   Serial.println("Error in WiFi connection");
-
- }
-
-  delay(10000);  //Send a request every 10 seconds
-
+void write_pwm_yaw(float pwm) {
+  bool dir = pwm > 1 ? LOW : HIGH;
+  Serial.println(dir);
+  ledcWrite(YAW_PWM_CHANNEL, abs(pwm));
+  digitalWrite(YAW_DIR_1, dir);
+  digitalWrite(YAW_DIR_2, !dir);
+  Serial.println(abs(pwm));
 }
