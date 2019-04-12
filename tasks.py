1f086a474636c91e3bb290a7128602ffa504ca60
protonauta <pinedacesar94@hotmail.com>
2019-04-12 13:25:59 -0500

    SR-016 | Add sed command for setting module id from shell command
    	modified:   tasks.py

--- tasks.py
+++ tasks.py
@@ -6,28 +6,24 @@ This task compiles an arduino sketch file (*.ino) and uploads to
 the Arduino in the given port.
 '''
 @task
-def upload(context, module=0, device='USB0'):
+def upload(context, module=1, device='USB0'):
     modules = range(1,7)
     ARDUINO_PATH = '~/Software/arduino-1.8.7/'
-    SKETCH_PATH = '~/snake_robot/Arduino/'
+    SKETCH_PATH = 'Arduino/module/module.ino'
     ARDUINO_PORT = f'/dev/tty{device}'
     if not module:
-        print(colored('Module number(1-7) must be specified', 'red'))
+        print(colored('Module number (1-7) must be specified', 'red'))
         return
     if not module in modules:
         print(colored(f"Unknown module '{module}'", 'yellow'))
         return
+    # Change module ID sketch
+    command = (f"sed -i 's/MOD_ID = .*/MOD_ID = \"{module}\";/g'"
+               f" {SKETCH_PATH}")
+    print(colored("Changing sketch module id . . .", 'yellow'))
+    run(command)
+    # Upload scketch
     command = (f'{ARDUINO_PATH}./arduino '
-              f'--upload {SKETCH_PATH}/module{module}/module{module}.ino '
+              f'--upload {SKETCH_PATH} '
               f'--port {ARDUINO_PORT}')
-    print(run(command))
-
-@task
-def installRosDep(context):
-    BASE = 'ros-melodic-'
-    ros_packages = ['joy']
-    packages = ''
-    for package in ros_packages:
-        packages += f'{BASE}{package} '
-    command = f'sudo apt install {packages}'
     print(run(command))
